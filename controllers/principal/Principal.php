de67234e931c90a728acd98aaaf989ced7a49d0a
Jan Carlos Guevara Perdomo <jguevara8076@cue.edu.co>
2025-08-24 21:26:38 -0500

    Actualización del proyecto: Nuevos controladores, modelos y configuración de la aplicación

---
+++ controllers/principal/Principal.php
@@ -0,0 +1,10 @@
+<?php
+class Principal extends Controller {
+    public function __construct() {
+        parent::__construct();
+    }
+   public function index() {
+    $data = $this->model->getPrueba();
+       echo $data;
+   }
+}
